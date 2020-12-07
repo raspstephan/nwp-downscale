78896c40d6c598ec08f3d01929e4dfaf8c1fecb6
Stephan Rasp <raspstephan@gmail.com>
2020-12-07 11:27:07 +0000

    Added tigge ensemble downloads; started with holoviews visualization

--- src/download_tigge.py
+++ src/download_tigge.py
@@ -1,9 +1,9 @@
 from ecmwfapi import ECMWFDataServer
 from fire import Fire
 
-def main(start_date, stop_date, fn):
+def main(start_date, stop_date, fn, ensemble=False):
     server = ECMWFDataServer()
-    server.retrieve({
+    request = {
         "class": "ti",
         "dataset": "tigge",
         "date": f"{start_date}/to/{stop_date}",
@@ -16,7 +16,11 @@ def main(start_date, stop_date, fn):
         "type": "cf",
         "target": fn,
         # "format": "netcdf"
-    })
+    }
+    if ensemble:
+        request['number'] = "1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50"
+        request['type'] = 'pf'
+    server.retrieve(request)
     
 if __name__ == '__main__':
     Fire(main)
