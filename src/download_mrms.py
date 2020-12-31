726d57b76d86b293c989eb9401b7793a71439ed2
Stephan Rasp <raspstephan@gmail.com>
2020-12-31 09:02:21 +0000

    Improved download script for cached MRMS data

--- src/download_mrms.py
+++ src/download_mrms.py
@@ -2,38 +2,39 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import os
+import xarray as xr
 import shutil
 import gzip
 from zipfile import ZipFile
 from fire import Fire
 import urllib.request
 
-default_variables = [
-    'MultiSensor_QPE_01H_Pass1',
-    'MultiSensor_QPE_03H_Pass1',
-    'MultiSensor_QPE_06H_Pass1',
-    'MultiSensor_QPE_01H_Pass2',
-    'MultiSensor_QPE_03H_Pass2',
-    'MultiSensor_QPE_06H_Pass2',
-    'RadarOnly_QPE_01H',
-    'RadarOnly_QPE_03H',
-    'RadarOnly_QPE_06H',
+variables = [
+    'MultiSensor_QPE_{}H_Pass1',
+    'MultiSensor_QPE_{}H_Pass2',
+    'RadarOnly_QPE_{}H',
+    'RadarAccumulationQualityIndex_{}H',
 ]
-
-def download_loop(start_date, stop_date, tmp_path, save_path, variables=None):
-    dates = pd.DatetimeIndex(np.arange(start_date, stop_date, dtype='datetime64[h]'))
-    for d in tqdm(dates):
-        download_and_extract(d.year, d.month, d.day, d.hour, tmp_path, save_path, variables=variables)
         
         
-def download_and_extract(year, month, day, hour, tmp_path, save_path, delete=True, zip_fn=None,
-                         variables=None):
+def download_and_extract(year, month, day, hour, tmp_path, save_path, dt, delete=True, zip_fn=None, delete_grib=True):
     zip_fn = zip_fn or download_nrms_from_cache(year, month, day, hour, tmp_path)
     unzip_file(zip_fn, tmp_path)
-    variables = variables or default_variables
     for v in variables:
+        v = v.format(str(dt).zfill(2))
         try:
-            move_relevant_file(year, month, day, hour, v, tmp_path, save_path)
+            grib_fn = move_relevant_file(year, month, day, hour, v, tmp_path, save_path)
+            ds = xr.open_dataset(grib_fn, engine='cfgrib')
+            ds = ds.rename({
+                'paramId_0': 'tp',
+                'latitude': 'lat',
+                'longitude': 'lon',
+            }).drop(['valid_time', 'heightAboveSea', 'step']).expand_dims(dim='time')
+            nc_fn = grib_fn.rstrip('.grib2') + '.nc'
+            ds.to_netcdf(nc_fn)
+            ds.close()
+            if delete_grib:
+                os.remove(grib_fn)
         except FileNotFoundError:
             print(f'File not found: {v}')
     if delete:
@@ -53,6 +54,7 @@ def move_relevant_file(year, month, day, hour, variable, tmp_path, save_path):
     os.makedirs(f'{save_path}/{variable}', exist_ok=True)
     save_fn  = f'{save_path}/{variable}/{fn}'
     gunzip_file(gz_fn, save_fn)
+    return save_fn
 
     
 def gunzip_file(gz_fn, out_fn=None):
@@ -78,6 +80,12 @@ def download_nrms_from_cache(year, month, day, hour, path):
     urllib.request.urlretrieve(url, zip_fn)
     return zip_fn
 
+
+def download_loop(start_date, stop_date, dt, tmp_path, save_path, delete_grib=True):
+    dates = pd.DatetimeIndex(np.arange(start_date, stop_date, dt, dtype='datetime64[h]'))
+    for d in tqdm(dates):
+        download_and_extract(
+            d.year, d.month, d.day, d.hour, tmp_path, save_path, dt, delete_grib=delete_grib)
 
 if __name__ == '__main__':
     Fire(download_loop)
