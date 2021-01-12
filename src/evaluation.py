e8865fda5308eca4e53446b70e8d69e8d33e0e70
M.Hirt <m.hirt@lmu.de>
2021-01-12 21:28:12 +0100

    FSS metric added for evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,6 +2,7 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import xskillscore as xs
+from dask.diagnostics import ProgressBar
 
 
 """ Evaluation functions and classes (??) 
@@ -22,6 +23,92 @@ General workflow structure:
     b) Ensemble: CRPS, Rank histograms
     c) Realism: precip amount histogram/spectra, cell size distributions
 """
+
+#------------ FSS-functions ( copied and adapted from L. Scheck.) ----------------
+# -*- coding: utf-8 -*-
+#
+#  K E N D A P Y . S C O R E _ F S S
+#  compute Fractions (skill) score and related quantities (copied from L. Scheck)
+#
+#  Almost completely adapted from
+#  Faggian, Roux, Steinle, Ebert (2015) "Fast calculation of the fractions skill score"
+#  MAUSAM, 66, 3, 457-466
+#
+#"""
+#.. module:: score_fss
+#:platform: Unix
+#:synopsis: Compute the fraction skill score (2D).
+#.. moduleauthor:: Nathan Faggian <n.faggian@bom.gov.au>
+#"""
+
+def compute_integral_table(field) :
+    return field.cumsum(1).cumsum(0)
+
+
+def integral_filter(field, n, table=None) :
+    """
+    Fast summed area table version of the sliding accumulator.
+    :param field: nd-array of binary hits/misses.
+    :param n: window size.
+    """
+    w = n // 2
+    if w < 1. :
+        return field
+    if table is None:
+        table = compute_integral_table(field)
+
+    r, c = np.mgrid[ 0:field.shape[0], 0:field.shape[1] ]
+    r = r.astype(np.int)
+    c = c.astype(np.int)
+    w = np.int(w)
+    r0, c0 = (np.clip(r - w, 0, field.shape[0] - 1), np.clip(c - w, 0, field.shape[1] - 1))
+    r1, c1 = (np.clip(r + w, 0, field.shape[0] - 1), np.clip(c + w, 0, field.shape[1] - 1))
+    integral_table = np.zeros(field.shape).astype(np.int64)
+    integral_table += np.take(table, np.ravel_multi_index((r1, c1), field.shape))
+    integral_table += np.take(table, np.ravel_multi_index((r0, c0), field.shape))
+    integral_table -= np.take(table, np.ravel_multi_index((r0, c1), field.shape))
+    integral_table -= np.take(table, np.ravel_multi_index((r1, c0), field.shape))
+    return integral_table
+
+
+def fss(fcst, obs, threshold, window, fcst_cache=None, obs_cache=None):
+    """
+    Compute the fraction skill score using summed area tables .
+    :param fcst: nd-array, forecast field.
+    :param obs: nd-array, observation field.
+    :param window: integer, window size.
+    :return: tuple of FSS numerator, denominator and score.
+    """
+    fhat = integral_filter( fcst > threshold, window, fcst_cache )
+    ohat = integral_filter( obs  > threshold, window, obs_cache  )
+
+    num = np.nanmean(np.power(fhat - ohat, 2))
+    denom = np.nanmean(np.power(fhat, 2) + np.power(ohat, 2))
+    return num, denom, 1.-num/denom
+
+def fss_frame(fcst, obs, windows, levels):
+    """
+    Compute the fraction skill score data-frame.
+    :param fcst: nd-array, forecast field.
+    :param obs: nd-array, observation field.
+    :param window: list, window sizes.
+    :param levels: list, threshold levels.
+    :return: list, dataframes of the FSS: numerator,denominator and score.
+    """
+    num_data, den_data, fss_data = [], [], []
+    #print(fcst.shape)
+    #print(obs.shape)
+    for level in levels:
+        ftable = compute_integral_table( fcst > level )
+        otable = compute_integral_table( obs  > level )
+        _data = [fss(fcst, obs, level, w, ftable, otable) for w in windows]
+        num_data.append([x[0] for x in _data])
+        den_data.append([x[1] for x in _data])
+        fss_data.append([x[2] for x in _data])
+    return np.array(fss_data) #pd.DataFrame(fss_data, index=levels, columns=windows)
+# ----------------------------------
+
+
 
 def compare_fields(X, y_G = None, y_b = None, y=None, 
                    levels = np.arange(0,10,0.1), cmap='viridis', 
@@ -117,14 +204,19 @@ def get_baseline(X, y, kind = 'interpol',
     assert y_baseline.shape == y.shape, 'y_baseline and y do not have the same size!'
     return y_baseline
     
-def compute_eval_metrics(fcst, obs, eval_mask, metrics =['RMSE']): 
+def compute_eval_metrics(fcst, obs, eval_mask = None, metrics = ['RMSE', 'FSS'],
+                     fss_scales=[41,61], fss_thresholds = [1., 5.] ): 
     """ Function to compute evaluation metrics to compare a forecast with observations
     fcst: xr-array of the forecast, e.g. the interpolation baseline or the downscaled forecast
     obs: xr-array fo observations, i.e. radar data 
-    eval_mask: boolean mask to apply the evaluation on, i.e. radar quality mask
+    eval_mask: 2d boolean mask to apply the evaluation on, i.e. radar quality mask
     metrics: list of metrics to consider
     
+    fss_scales: list of spatial scales to use for the FSS calculation
+    fss_thresholds: list of precip thresholds to use for the FSS calculation
+    
     Returns: xr-dataset with different metrics as different variables   
+        Note that this function utilizes dask and returns xarrays with dask arrays. Use
     """
     
     
@@ -136,13 +228,33 @@ def compute_eval_metrics(fcst, obs, eval_mask, metrics =['RMSE']):
     fcst = fcst.chunk({'time':1})
     obs = obs.chunk({'time':1}) 
     
-    
+    # apply eval mask: 
+    if eval_mask is not None:
+        fcst = fcst.where(eval_mask)
+        obs = obs.where(eval_mask)
+        
     metrics_ds = xr.Dataset()
     # RMSE 
     if 'RMSE' in metrics: 
-        rmse = xs.rmse(fcst.where(eval_mask), 
-            obs.where(eval_mask), dim=['lon', 'lat'], skipna=True)
+        rmse = xs.rmse(fcst, 
+            obs, dim=['lon', 'lat'], skipna=True)
         metrics_ds['RMSE'] = rmse
+        
+    if 'FSS' in metrics: # maybe there is a way to implement this faster?
+        from dask.diagnostics import ProgressBar
+
+        fss_da = xr.apply_ufunc(fss_frame, fcst, obs, input_core_dims=[[ 'lat', 'lon'], ['lat', 'lon']],
+                       output_core_dims=[['thresholds','scales']], 
+                       output_dtypes=[fcst.dtype],
+                       dask_gufunc_kwargs = dict(output_sizes= {'scales':len(fss_scales), 'thresholds': len(fss_thresholds)},),
+                       vectorize =True, dask='parallelized',
+                       kwargs = dict(windows=fss_scales, levels = fss_thresholds))
+        fss_da['thresholds'] = np.array(fss_thresholds)
+        fss_da['scales'] = np.array(fss_scales)
+        #with ProgressBar(minimum=1): 
+            #fss_da = fss_da.compute()
+        metrics_ds['FSS'] = fss_da
+    
     return metrics_ds
 
 def main(lead_time = 12): 
@@ -172,7 +284,9 @@ def main(lead_time = 12):
     eval_mask['lon']=mrms.lon
 
     # 4. compute metrics: 
-    compute_eval_metrics(baseline, mrms, eval_mask )
+    metrics = compute_eval_metrics(baseline, mrms, eval_mask )
+    with ProgressBar():
+        metrics.load() # execute fss computation    
 
 
 if __name__ == '__main__':
