5e90d983cea53228be0531cb53ddefec1be92560
raspstephan <raspstephan@gmail.com>
2021-02-08 14:13:15 +0000

    Modified sync bash scripts

--- sync_from_cpu.sh
+++ sync_from_cpu.sh
@@ -1,1 +1,2 @@
-rsync -avr nwp-downscale@104.40.194.42:/datadrive /datadrive 
+rsync -avr nwp-downscale@104.40.194.42:/datadrive/mrms/4km/ /datadrive/mrms/4km/
+rsync -avr nwp-downscale@104.40.194.42:/datadrive/tigge/32km/ /datadrive/tigge/32km/
