7f4d43f35908e891193fc2b782bbda11ceabcf00
raspstephan <raspstephan@gmail.com>
2021-03-23 08:21:33 +0000

    Small changes and additions to models and trainer

--- src/trainer.py
+++ src/trainer.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from .utils import tqdm, device
+from .utils import tqdm, device, plot_sample
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -99,9 +99,8 @@ class Trainer():
         plt.legend()
 
 
-
 ### GANs
-def get_gradient(crit, real, fake, epsilon):
+def get_gradient(crit, X, real, fake, epsilon):
     '''
     Return the gradient of the critic's scores with respect to mixes of real and fake images.
     Parameters:
@@ -116,7 +115,7 @@ def get_gradient(crit, real, fake, epsilon):
     mixed_images = real * epsilon + fake * (1 - epsilon)
 
     # Calculate the critic's scores on the mixed images
-    mixed_scores = crit(mixed_images)
+    mixed_scores = crit([X, mixed_images])
     
     # Take the gradient of the scores with respect to the images
     gradient = torch.autograd.grad(
@@ -157,117 +156,13 @@ def gradient_penalty(gradient):
     #### END CODE HERE ####
     return penalty
 
-class WGANTrainer():
-    """Implements a Keras-style fit function and tracks train/valid losses"""
-    def __init__(self, gen, disc, gen_optimizer, disc_optimizer,  dl_train, dl_valid=None, 
-                 valid_every_epochs=1, disc_repeats=1, gp_lambda=10):
-        self.gen = gen
-        self.disc = disc
-        self.gen_optimizer = gen_optimizer
-        self.disc_optimizer = disc_optimizer
-        self.dl_train = dl_train
-        self.dl_valid = dl_valid
-        self.valid_every_epochs = valid_every_epochs
-        self.criterion = nn.BCELoss()
-        self.disc_repeats = disc_repeats
-        self.gp_lambda = gp_lambda
-        
-        self.epoch = 0
-        self.train_gen_losses = []
-        self.train_mse = []
-        self.train_disc_losses = []
-        self.train_epochs = []
-#         self.valid_losses = []
-#         self.valid_epochs = []
-        
-    def fit(self, epochs):
-
-        # Epoch loop
-        for epoch in range(1, epochs+1):
-
-            prog_bar = tqdm.tqdm(total=len(self.dl_train), desc=f'Epoch {epoch}')
-            train_loss, valid_loss = 0, 0
-
-            # Train
-            for i, (X, y) in enumerate(self.dl_train):
-                X = X.to(device); real = y.to(device)
-                bs = X.shape[0]
-                
-                mean_disc_loss = 0
-                for _ in range(self.disc_repeats):
-                    # Train discriminator
-                    self.disc_optimizer.zero_grad()
-
-                    fake = self.gen(X)
-                    preds_real = self.disc(real)
-                    preds_fake = self.disc(fake.detach())
-                    
-                    epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
-                    gradient = get_gradient(disc, real, fake.detach(), epsilon)
-                    gp = gradient_penalty(gradient)
-                    
-                    disc_loss = -torch.mean(preds_real) + torch.mean(preds_fake) + self.gp_lambda * gp
-
-                    mean_disc_loss += disc_loss.item() / self.disc_repeats
-                    disc_loss.backward(retain_graph=True)
-                    self.disc_optimizer.step()
-                
-                
-                # Train generator
-                self.gen_optimizer.zero_grad()
-                
-                fake = self.gen(X)
-                preds_fake = self.disc(fake)
-                
-                mse = nn.MSELoss()(fake, real).item()
-                
-                gen_loss = -torch.mean(preds_fake)
-                gen_loss.backward()
-                self.gen_optimizer.step()
-                
-
-                prog_bar.update()
-#                 train_gen_loss += (loss.item() - train_loss) / (i+1)
-                self.train_gen_losses.append(gen_loss.item())
-                self.train_mse.append(mse)
-                self.train_disc_losses.append(mean_disc_loss)
-                prog_bar.set_postfix({
-                    'train_gen_loss': gen_loss.item(),
-                    'train_disc_loss': disc_loss.item(),
-                    'mse': mse
-                })
-#             self.train_gen_losses.append(gen_loss)
-            self.train_epochs.append(self.epoch)
-
-#             if (self.epoch-1) % self.valid_every_epochs == 0:
-#                 # Valid
-#                 for i, (X, y) in enumerate(self.dl_valid):
-#                     X = X.to(device); y = y.to(device)
-#                     y_hat = self.model(X)
-#                     loss = self.criterion(y_hat, y)
-
-#                     valid_loss += (loss.cpu().detach().numpy() - valid_loss) / (i+1)
-#                 self.valid_losses.append(valid_loss)
-#                 self.valid_epochs.append(self.epoch)
-
-#                 prog_bar.set_postfix({'train_loss': train_loss, 'valid_loss': valid_loss}) 
-#                 prog_bar.close()
-        
-            self.epoch += 1
-        
-    def plot_losses(self, plot_valid=True):
-        plt.plot(self.train_epochs, self.train_losses, label='Train')
-        if plot_valid: plt.plot(self.valid_epochs, self.valid_losses, label='Valid')
-        plt.legend()
-        
-        
-        
-        
         
 class GANTrainer():
     """Implements a Keras-style fit function and tracks train/valid losses"""
     def __init__(self, gen, disc, gen_optimizer, disc_optimizer,  dl_train, dl_valid=None, 
-                 valid_every_epochs=1, disc_repeats=1, gp_lambda=None, l_loss=None, l_lambda=20, dloss_type = 'hinge'):
+                 valid_every_epochs=1, disc_repeats=1, gen_repeats=1,
+                 gp_lambda=None, l_loss=None, l_lambda=20, dloss_type='hinge',
+                 save_dir=None, plot=False, plotting_sample=None):
         """ GAN trainer that includes options to set e.g. hinge loss, gradient penalty, ... 
         """
         self.gen = gen
@@ -287,7 +182,11 @@ class GANTrainer():
             self.l_loss = None
         self.l_lambda = l_lambda
         self.disc_repeats = disc_repeats
+        self.gen_repeats = gen_repeats
         self.gp_lambda = gp_lambda
+        self.save_dir = save_dir
+        self.plot = plot
+        self.plotting_sample = plotting_sample
         
         self.epoch = 0
         self.train_gen_losses = []
@@ -340,52 +239,55 @@ class GANTrainer():
                 
                 
                 # Train generator
-                self.gen_optimizer.zero_grad()
-                
-                fake = self.gen(X)
-                preds_fake = self.disc([X, fake])
-                
-                mse = nn.MSELoss()(fake, real).item()   # For diagnostics only
-                
-                gen_loss = -torch.mean(preds_fake)
-                if self.l_loss:
-                    l_loss = self.l_loss(fake, real)
-                    gen_loss += self.l_lambda * l_loss
-                gen_loss.backward()
-                self.gen_optimizer.step()
-                
+                mean_gen_loss = 0
+                mean_mse = 0
+                mean_l_loss = 0
+                for _ in range(self.gen_repeats):
+                    self.gen_optimizer.zero_grad()
 
+                    fake = self.gen(X)
+                    preds_fake = self.disc([X, fake])
+
+                    mse = nn.MSELoss()(fake, real).item()   # For diagnostics only
+
+                    gen_loss = -torch.mean(preds_fake)
+                    if self.l_loss:
+                        l_loss = self.l_loss(fake, real)
+                        gen_loss += self.l_lambda * l_loss
+                    gen_loss.backward()
+                    self.gen_optimizer.step()
+                    
+                    mean_gen_loss += gen_loss.item() / self.gen_repeats
+                    mean_mse += mse / self.gen_repeats
+                    mean_l_loss += l_loss.item() / self.gen_repeats
+               
                 prog_bar.update()
 #                 train_gen_loss += (loss.item() - train_loss) / (i+1)
-                self.train_gen_losses.append(gen_loss.item())
+                self.train_gen_losses.append(mean_gen_loss)
                 self.train_mse.append(mse)
                 self.train_disc_losses.append(mean_disc_loss)
                 postfix = {
-                    'train_gen_loss': gen_loss.item(),
-                    'train_disc_loss': disc_loss.item(),
-                    'mse': mse,
+                    'train_gen_loss': mean_gen_loss,
+                    'train_disc_loss': mean_disc_loss,
+                    'mse': mean_mse,
                 }
-                if self.l_loss: postfix['l_loss'] = l_loss.item()
+                if self.l_loss: postfix['l_loss'] = mean_l_loss
                 prog_bar.set_postfix(postfix)
             self.train_epochs.append(self.epoch)
 
-#             if (self.epoch-1) % self.valid_every_epochs == 0:
-#                 # Valid
-#                 for i, (X, y) in enumerate(self.dl_valid):
-#                     X = X.to(device); y = y.to(device)
-#                     y_hat = self.model(X)
-#                     loss = self.criterion(y_hat, y)
-
-#                     valid_loss += (loss.cpu().detach().numpy() - valid_loss) / (i+1)
-#                 self.valid_losses.append(valid_loss)
-#                 self.valid_epochs.append(self.epoch)
-
-#                 prog_bar.set_postfix({'train_loss': train_loss, 'valid_loss': valid_loss}) 
-#                 prog_bar.close()
-        
+            if self.save_dir:
+                torch.save(gen.state_dict(), f'{self.save_dir}/gen_{epoch}.pt')
+                torch.save(disc.state_dict(), f'{self.save_dir}/disc_{epoch}.pt')
+            if self.plot:
+                if self.plotting_sample is None:
+                    X_sample, y_sample = X, y
+                else:
+                    X_sample, y_sample = self.plotting_sample
+                plot_sample(X_sample, y_sample, self.gen)
+                plt.show()
             self.epoch += 1
         
-    def plot_losses(self, plot_valid=True):
-        plt.plot(self.train_epochs, self.train_losses, label='Train')
-        if plot_valid: plt.plot(self.valid_epochs, self.valid_losses, label='Valid')
-        plt.legend()
+    # def plot_losses(self, plot_valid=True):
+    #     plt.plot(self.train_epochs, self.train_losses, label='Train')
+    #     if plot_valid: plt.plot(self.valid_epochs, self.valid_losses, label='Valid')
+    #     plt.legend()
--- src/utils.py
+++ src/utils.py
@@ -11,13 +11,45 @@ def isnotebook():
         return False      # Probably standard Python interpreter
 
 import torch
+import torch.nn as nn
 if torch.cuda.is_available():
     device = torch.device("cuda") 
 else:
     device = torch.device("cpu")
 
 # if isnotebook():
+#     print('In notebook')
 #     import tqdm.notebook as tqdm
 # else:
+#     print('Not in notebook')
 #     import tqdm
-import tqdm
+import tqdm.notebook as tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_sample(X, y, gen, i=0):
+    preds = gen(X.to(device)).detach().cpu().numpy()
+    lr = X[i, 0].detach().cpu().numpy()
+    hr = y[i, 0].detach().cpu().numpy()
+    pred = preds[i, 0]
+    
+    mn = np.min([np.min(lr), np.min(hr), np.min(pred)])
+    mx = np.max([np.max(lr), np.max(hr), np.max(pred)])
+    
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+    im = ax1.imshow(lr, vmin=mn, vmax=mx, cmap='gist_ncar_r')
+    plt.colorbar(im, ax=ax1, shrink=0.7)
+    im = ax2.imshow(pred, vmin=mn, vmax=mx, cmap='gist_ncar_r')
+    plt.colorbar(im, ax=ax2, shrink=0.7)
+    im = ax3.imshow(hr, vmin=mn, vmax=mx, cmap='gist_ncar_r')
+    plt.colorbar(im, ax=ax3, shrink=0.7)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def weights_init_normal(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
