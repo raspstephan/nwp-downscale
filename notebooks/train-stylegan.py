added leinstyle gen and disc, fixed mixed precision error in unconditional style gan

notebooks/train-stylegan.py
@@ -11,11 +11,6 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
-# from src.models import GANs, gens, discs
-# # from ilan_src.models import *
-# from src.dataloader import *
-# from src.utils import *
-
 from catalyst.data.sampler import DistributedSamplerWrapper
 
 import xarray as xr
@@ -64,56 +59,58 @@ def train(input_args):
     sys.path.append(model_dir)
     print("sys path:", sys.path)
     
-    from run_src.models import GANs, gens, discs
-    from run_src.dataloader import TiggeMRMSDataset
+    from src.stylegan import StyleGan2, Dataset
+    from src.dataloader import TiggeMRMSDataset
 #     from run_src.utils import *
     
     print("Args loaded")
     # set seed
     torch.manual_seed(args.seed)
-          
-    args.gan_hparams['generator'] = gens[args.gan_hparams['generator']]
-    args.gan_hparams['discriminator'] = discs[args.gan_hparams['discriminator']]
 
     ## Load Data and set data params
     
     print("Loading data ... ")
+#     ds_valid = Dataset('/home/jupyter/data/stylegan2', 64)
     ds_train = pickle.load(open(args.data_hparams['train_dataset_path'], "rb"))
     ds_valid = pickle.load(open(args.data_hparams['valid_dataset_path'], "rb"))
 
+#     import torchvision.datasets as datasets
+#     import torchvision.transforms as transforms
+#     transforms = transforms.Compose(
+#     [
+#         transforms.Resize(128),
+#         transforms.ToTensor(),
+#         transforms.Normalize(
+#             [0.5 for _ in range(1)], [0.5 for _ in range(1)]
+#         ),
+#     ]
+#     )
+#     ds_valid = datasets.MNIST(root="dataset/", transform=transforms, download=True)
+
+    
     sampler_train = torch.utils.data.WeightedRandomSampler(ds_train.compute_weights(), len(ds_train))
-    sampler_train = DistributedSamplerWrapper(sampler_train, num_replicas = len(args.train_hparams['gpus']) if type(args.train_hparams['gpus'])==list else  args.train_hparams['gpus'], rank = 0)
+    sampler_train = DistributedSamplerWrapper(sampler_train, num_replicas = args.train_hparams['gpus'], rank = 0)
+
+
     sampler_valid = torch.utils.data.WeightedRandomSampler(ds_valid.compute_weights(), len(ds_valid))
-    sampler_valid = DistributedSamplerWrapper(sampler_valid, num_replicas = len(args.train_hparams['gpus']) if type(args.train_hparams['gpus'])==list else  args.train_hparams['gpus'], rank = 0)
+    sampler_valid = DistributedSamplerWrapper(sampler_valid, num_replicas = args.train_hparams['gpus'], rank = 0)
     
-    if type(args.train_hparams['gpus']) == list:
-        batch_size = args.train_hparams['batch_size']//len(args.train_hparams['gpus'])
-    else:
-        batch_size = args.train_hparams['batch_size']//args.train_hparams['gpus']
+    batch_size = args.train_hparams['batch_size']//args.train_hparams['gpus']
     
     
-    dl_train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, sampler=sampler_train, num_workers=16)
-    dl_valid = torch.utils.data.DataLoader(ds_valid, batch_size=batch_size, sampler=sampler_valid, num_workers=16)
+    dl_train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, sampler=sampler_train, num_workers=16, drop_last = True)
 
+    dl_valid = torch.utils.data.DataLoader(ds_valid, batch_size=batch_size, sampler=sampler_valid, num_workers=16, drop_last = True)
     
-    args.gan_hparams['val_hparams']['ds_max'] = ds_train.maxs.tp.values
-    args.gan_hparams['val_hparams']['ds_min'] = ds_train.mins.tp.values
-    args.gan_hparams['val_hparams']['tp_log'] = ds_train.tp_log
+#     dl_valid = torch.utils.data.DataLoader(ds_valid, batch_size=batch_size, num_workers=16)
 
-    del ds_train
-    del ds_valid
     
     print("Data loading complete")
     ## Load Model
     if input_args.ckpt_path:
-        model = GANs[args.gan].load_from_checkpoint(input_args.ckpt_path)
-        model.loss_hparams = args.gan_hparams['loss_hparams']
-        model.opt_hparams = args.gan_hparams['opt_hparams']
-        model.noise_shape = args.gan_hparams['noise_shape']
-        print(model.opt_hparams['disc_lr'])
-        print(model.opt_hparams['gen_lr'])
+        model = StyleGan2().load_from_checkpoint(input_args.ckpt_path)
     else:
-        model = GANs[args.gan](**args.gan_hparams)
+        model = StyleGan2()
 
     ## Define trainer and logging
 
@@ -124,8 +121,7 @@ def train(input_args):
     tb_logger = pl_loggers.TensorBoardLogger(save_dir = '../logs/',
                                              name = args.save_hparams['run_name'], 
                                              version = args.save_hparams['run_number'])
-    
-    pickle.dump(args, open(save_dir+args.save_hparams['run_name']+str(args.save_hparams['run_number']) + '/experiment_args.json', 'wb') )
+
     
     if input_args.ckpt_path:
         trainer = pl.Trainer(accelerator='ddp', 
@@ -138,20 +134,23 @@ def train(input_args):
                          resume_from_checkpoint = input_args.ckpt_path
                         )
     else:
-        trainer = pl.Trainer(accelerator='ddp', 
-                         precision=16, gpus = args.train_hparams['gpus'], 
+        trainer = pl.Trainer(
+                            accelerator='ddp', 
+                         precision=16, 
+                         gpus = args.train_hparams['gpus'], 
                          max_epochs = args.train_hparams['epochs'], 
                          callbacks=[checkpoint_callback], 
                          replace_sampler_ddp = False, 
                          check_val_every_n_epoch=20, 
                          logger = tb_logger, 
+#                          gradient_clip_val=1.0
 #                          auto_select_gpus=True
                         )
                          
     print("Training model...")
     
     # Train
-    trainer.fit(model, dl_train, dl_valid)    
+    trainer.fit(model, dl_train)    
     
 
 if __name__ == '__main__':

src/models.py
@@ -772,8 +772,9 @@ class LeinGen2(nn.Module):
             if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):#, nn.BatchNorm2d)):
 #                 nn.init.normal_(m.weight.data, 0.0, 0.02)
                 nn.init.kaiming_normal_(m.weight.data)
-            
-                                     
+    
+    
+
 class LeinDisc(nn.Module):
     def __init__(self, input_channels=1, nonlin = 'leaky_relu'):
         super(LeinDisc, self).__init__()
@@ -2042,6 +2043,188 @@ class BaseGAN2(LightningModule):
         
         return crps
     
+
+from .stylegan import Conv2dWeightModulateNoStyle, EqualizedLinear, EqualizedConv2d, EqualizedWeight
+from .stylegan import UpSample as StyleUpSample
+from .stylegan import DownSample as StyleDownSample
+
+
+class LeinStyleGen(nn.Module):
+    def __init__(self, input_channels=1):
+        super(LeinStyleGen, self).__init__()
+        self.embed = nn.Sequential(EqualizedConv2d(input_channels,64, kernel_size=3), nn.LeakyReLU(negative_slope=0.02), 
+                                      LeinStyleResBlock(conv_block = EqConvBlock, in_planes=64, planes=128,  nonlin = 'leaky_relu'), 
+                                      LeinStyleResBlock(conv_block = EqConvBlock, in_planes=128, planes=255, nonlin = 'leaky_relu'))
+        
+        self.process = nn.Sequential(LeinStyleResBlock(conv_block = EqConvBlock, in_planes=256, planes=256,  nonlin = 'leaky_relu'), 
+                                      LeinStyleResBlock(conv_block = EqConvBlock, in_planes=256, planes=256, nonlin = 'leaky_relu'), 
+                            #         self.b3 = BasicBlock(in_planes=256, planes=256, stride=1, nonlin = 'relu')
+                            #         self.b4 = BasicBlock(in_planes=256, planes=256, stride=1, nonlin = 'leaky_relu')
+                                        )
+        self.upscale = nn.Sequential(LeinStyleResBlock(conv_block = EqConvBlock, in_planes=256, planes=256,  nonlin = 'leaky_relu'),
+                                     StyleUpSample(),
+                                     LeinStyleResBlock(conv_block = EqConvBlock, in_planes=256, planes=128,  nonlin = 'leaky_relu'),
+                                     StyleUpSample(),
+                                     LeinStyleResBlock(conv_block = EqConvBlock, in_planes=128, planes=64, nonlin = 'leaky_relu'),
+                                     StyleUpSample(),
+                                     LeinStyleResBlock(conv_block = EqConvBlock, in_planes=64, planes=32,  nonlin = 'leaky_relu'))
+        
+        self.final = EqualizedConv2d(32,1, kernel_size=3)
+         
+    def forward(self, x, noise):
+        x = self.embed(x)
+        x = torch.cat((x,noise), axis=1)
+        x = self.process(x)
+#         print(x.shape)
+        x = self.upscale(x)
+        x = torch.sigmoid(self.final(x))
+        return x
+    
+class LeinStyleGen2(nn.Module):
+    def __init__(self, input_channels=1):
+        super(LeinStyleGen2, self).__init__()
+        self.embed = nn.Sequential(Conv2dWeightModulateNoStyle(input_channels,64, kernel_size=3), nn.LeakyReLU(negative_slope=0.02), 
+                                      LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=64, planes=128,  nonlin = 'leaky_relu'), 
+                                      LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=128, planes=255, nonlin = 'leaky_relu'))
+        
+        self.process = nn.Sequential(LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=256, planes=256,  nonlin = 'leaky_relu'), 
+                                      LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=256, planes=256, nonlin = 'leaky_relu'), 
+                            #         self.b3 = BasicBlock(in_planes=256, planes=256, stride=1, nonlin = 'relu')
+                            #         self.b4 = BasicBlock(in_planes=256, planes=256, stride=1, nonlin = 'leaky_relu')
+                                        )
+        self.upscale = nn.Sequential(LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=256, planes=256,  nonlin = 'leaky_relu'),
+                                     StyleUpSample(),
+                                     LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=256, planes=128,  nonlin = 'leaky_relu'),
+                                     StyleUpSample(),
+                                     LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=128, planes=64, nonlin = 'leaky_relu'),
+                                     StyleUpSample(),
+                                     LeinStyleResBlock(conv_block = ModulatedConvBlock, in_planes=64, planes=32,  nonlin = 'leaky_relu'))
+        
+        self.final = Conv2dWeightModulateNoStyle(32,1, kernel_size=3)
+         
+    def forward(self, x, noise):
+        x = self.embed(x)
+        x = torch.cat((x,noise), axis=1)
+        x = self.process(x)
+#         print(x.shape)
+        x = self.upscale(x)
+        x = torch.sigmoid(self.final(x))
+        return x
+    
+class ModulatedConvBlock(nn.Module):
+    def __init__(self, in_channels, channels, kernel_size = 3, norm=None, activation='leaky_relu', padding=1):
+        super(ModulatedConvBlock, self).__init__()
+        self.conv = Conv2dWeightModulateNoStyle(in_channels, channels, kernel_size=kernel_size)
+        self.norm = norm
+        self.activation = activation
+        
+    def forward(self, x):
+        
+        if self.norm=="batch":
+            x = nn.BatchNorm2d(in_channels)(x)
+        if self.activation == 'leaky_relu':
+            x = F.leaky_relu(x, negative_slope=0.2)
+        elif self.activation == 'relu':
+            x = F.relu(x)
+        x = self.conv(x)
+        return x               
+
+class LeinStyleResBlock(nn.Module):
+
+    def __init__(self, conv_block=ModulatedConvBlock, in_planes=256, planes=256, nonlin = 'relu', norm =None):
+        super(LeinStyleResBlock, self).__init__()
+        self.in_planes = in_planes
+        self.planes = planes
+        self.nonlin = nonlin
+        self.norm = norm
+        
+        shortcut_modules = []
+#         if self.stride>1:
+#             shortcut_modules.append(nn.AvgPool2d(self.stride))
+        if (self.planes != self.in_planes):
+                shortcut_modules.append(conv_block(self.in_planes, self.planes, 1,
+                    activation=False, padding=0))
+        
+        self.shortcut = nn.Sequential(*shortcut_modules)   
+        self.convblock1 = conv_block(self.in_planes, self.planes, 3,
+            norm=self.norm,
+            activation=self.nonlin)
+        self.convblock2 = conv_block(self.planes, self.planes, 3,
+            norm=self.norm,
+            activation=self.nonlin)
+        
+    def forward(self, x):
+        x_in = x
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x_in = self.shortcut(x_in)
+        x = x + x_in
+        return x
+
+
+
+
+class EqConvBlock(nn.Module):
+    def __init__(self, in_channels, channels, kernel_size = 3, norm=None, activation='leaky_relu', padding=1):
+        super(EqConvBlock, self).__init__()
+        self.conv = EqualizedConv2d(in_channels, channels, kernel_size=kernel_size, padding=padding)
+        self.norm = norm
+        self.activation = activation
+        
+    def forward(self, x):
+        
+        if self.norm=="batch":
+            x = nn.BatchNorm2d(in_channels)(x)
+        if self.activation == 'leaky_relu':
+            x = F.leaky_relu(x, negative_slope=0.2)
+        elif self.activation == 'relu':
+            x = F.relu(x)
+        x = self.conv(x)
+        return x
+
+    
+class LeinStyleDisc(nn.Module):
+    def __init__(self, input_channels=1, nonlin = 'leaky_relu'):
+        super(LeinStyleDisc, self).__init__()
+        hr_block = []
+        lr_block = []
+        lr_inplanes = input_channels
+        hr_inplanes = 1
+        for planes in [64, 128, 256]:
+            hr_block.append(LeinStyleResBlock(conv_block = EqConvBlock, in_planes = hr_inplanes, planes=planes, nonlin = nonlin))
+            hr_block.append(StyleDownSample())
+            lr_block.append(LeinStyleResBlock(conv_block = EqConvBlock, in_planes = lr_inplanes, planes=planes, nonlin = nonlin))
+            lr_inplanes=planes
+            hr_inplanes=planes
+        self.hr_block1 = nn.Sequential(*hr_block)
+        self.lr_block1 = nn.Sequential(*lr_block)
+        self.hr_block2 = nn.Sequential(LeinStyleResBlock(conv_block = EqConvBlock, in_planes=256, planes=256, nonlin = nonlin))#, block(in_planes=256, planes=256, stride=1, nonlin = nonlin))
+        self.lr_block2 = nn.Sequential(LeinStyleResBlock(conv_block = EqConvBlock, in_planes=512, planes=256, nonlin = nonlin))#,block(in_planes=256, planes=256, stride=1, nonlin = nonlin))
+        self.dense1 = EqualizedLinear(512, 256)
+        self.dense2 = EqualizedLinear(256, 1)
+        
+        
+        
+
+    def forward(self, X, y):
+        hr = self.hr_block1(y)
+        lr = self.lr_block1(X)
+        lr = torch.cat((lr,hr), axis=1)
+        hr = self.hr_block2(hr)
+        lr = self.lr_block2(lr)
+        hr = nn.AvgPool2d(16)(hr)
+        lr = nn.AvgPool2d(16)(lr)
+        out = torch.cat((torch.reshape(hr, (hr.shape[0],-1)), torch.reshape(lr, (lr.shape[0], -1))), axis=1)
+        out = F.leaky_relu(self.dense1(out), negative_slope=0.02)
+        out = self.dense2(out)
+        return out    
+    
+    
+    
+    
+    
+    
+    
 GANs = {
     'base':BaseGAN, 
     'wgan-gp':WGANGP, 
@@ -2054,11 +2237,14 @@ gens = {
     'broadleingen':BroadLeinGen, 
     'leinsagen':LeinSAGen, 
     'broadleinsagen':BroadLeinSAGen, 
+    'leinstylegen': LeinStyleGen,
+    'leinstylegen2': LeinStyleGen2
 }
 
 discs = {
     'leindisc':LeinDisc,
     'broadleindisc':BroadLeinDisc, 
     'leinsadisc':LeinSADisc, 
-    'broadleinsadisc': BroadLeinSADisc
+    'broadleinsadisc': BroadLeinSADisc, 
+    'leinstyledisc': LeinStyleDisc
 }

src/stylegan.py
@@ -305,7 +305,7 @@ class Conv2dWeightModulate(nn.Module):
     """
 
     def __init__(self, in_features: int, out_features: int, kernel_size: int,
-                 demodulate: float = True, eps: float = 1e-8):
+                 demodulate: float = True, eps: float = 1e-5):
         """
         * `in_features` is the number of features in the input feature map
         * `out_features` is the number of features in the output feature map
@@ -344,6 +344,70 @@ class Conv2dWeightModulate(nn.Module):
         #
         # The result has shape `[batch_size, out_features, in_features, kernel_size, kernel_size]`
         weights = weights * s
+
+        # Demodulate
+        if self.demodulate:
+            # $$\sigma_j = \sqrt{\sum_{i,k} (w'_{i, j, k})^2 + \epsilon}$$
+            sigma_inv = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + self.eps)
+            # $$w''_{i,j,k} = \frac{w'_{i,j,k}}{\sqrt{\sum_{i,k} (w'_{i, j, k})^2 + \epsilon}}$$
+            weights = weights * sigma_inv
+
+        # Reshape `x`
+        x = x.reshape(1, -1, h, w)
+
+        # Reshape weights
+        _, _, *ws = weights.shape
+        weights = weights.reshape(b * self.out_features, *ws)
+
+        # Use grouped convolution to efficiently calculate the convolution with sample wise kernel.
+        # i.e. we have a different kernel (weights) for each sample in the batch
+        x = F.conv2d(x, weights, padding=self.padding, groups=b)
+
+        # Reshape `x` to `[batch_size, out_features, height, width]` and return
+        return x.reshape(-1, self.out_features, h, w)
+    
+class Conv2dWeightModulateNoStyle(nn.Module):
+    """
+    ### Convolution with Weight Modulation and Demodulation
+    This layer scales the convolution weights by the style vector and demodulates by normalizing it.
+    """
+
+    def __init__(self, in_features: int, out_features: int, kernel_size: int,
+                 demodulate: float = True, eps: float = 1e-5):
+        """
+        * `in_features` is the number of features in the input feature map
+        * `out_features` is the number of features in the output feature map
+        * `kernel_size` is the size of the convolution kernel
+        * `demodulate` is flag whether to normalize weights by its standard deviation
+        * `eps` is the $\epsilon$ for normalizing
+        """
+        super().__init__()
+        # Number of output features
+        self.out_features = out_features
+        # Whether to normalize weights
+        self.demodulate = demodulate
+        # Padding size
+        self.padding = (kernel_size - 1) // 2
+
+        # [Weights parameter with equalized learning rate](#equalized_weight)
+        self.weight = EqualizedWeight([out_features, in_features, kernel_size, kernel_size])
+        # $\epsilon$
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor):
+        """
+        * `x` is the input feature map of shape `[batch_size, in_features, height, width]`
+        """
+
+        # Get batch size, height and width
+        b, _, h, w = x.shape
+        
+        # Get [learning rate equalized weights](#equalized_weight)
+        weights = self.weight()[None, :, :, :, :]
+        # $$w`_{i,j,k} = s_i * w_{i,j,k}$$
+        # where $i$ is the input channel, $j$ is the output channel, and $k$ is the kernel index.
+        #
+        # The result has shape `[batch_size, out_features, in_features, kernel_size, kernel_size]`
 
         # Demodulate
         if self.demodulate:
@@ -451,7 +515,7 @@ class DiscriminatorBlock(nn.Module):
         super().__init__()
         # Down-sampling and $1 \times 1$ convolution layer for the residual connection
         self.residual = nn.Sequential(DownSample(),
-                                      EqualizedConv2d(in_features, out_features, kernel_size=1))
+                                      EqualizedConv2d(in_features, out_features, kernel_size=1, padding=0))
 
         # Two $3 \times 3$ convolutions
         self.block = nn.Sequential(
@@ -509,7 +573,7 @@ class MiniBatchStdDev(nn.Module):
         # Calculate the standard deviation for each feature among `group_size` samples
         # $$\mu_{i} = \frac{1}{N} \sum_g x_{g,i} \\
         #   \sigma_{i} = \sqrt{\frac{1}{N} \sum_g (x_{g,i} - \mu_i)^2  + \epsilon}$$
-        std = torch.sqrt(grouped.var(dim=0) + 1e-8)
+        std = torch.sqrt(grouped.var(dim=0) + 1e-5)
         # Get the mean standard deviation
         std = std.mean().view(1, 1, 1, 1)
         # Expand the standard deviation to append to the feature map
@@ -633,7 +697,7 @@ class EqualizedConv2d(nn.Module):
     """
 
     def __init__(self, in_features: int, out_features: int,
-                 kernel_size: int, padding: int = 0):
+                 kernel_size: int, padding: int = 1):
         """
         * `in_features` is the number of features in the input feature map
         * `out_features` is the number of features in the output feature map
@@ -965,9 +1029,9 @@ class StyleGan2(LightningModule):
         return images, w
     
     def configure_optimizers(self):
-        generator_optimizer = optim.Adam(self.generator.parameters(), lr=self.learning_rate, betas=(0, 0.99))
-        discriminator_optimizer = optim.Adam(self.discriminator.parameters(), lr=self.learning_rate, betas=(0, 0.99))
-        mapping_network_optimizer = optim.Adam(self.mapping_network.parameters(), lr=self.mapping_network_learning_rate, betas=(0, 0.99))
+        generator_optimizer = optim.Adam(self.generator.parameters(), lr=self.learning_rate, betas=(0, 0.99), eps=1e-5)
+        discriminator_optimizer = optim.Adam(self.discriminator.parameters(), lr=self.learning_rate, betas=(0, 0.99), eps=1e-5)
+        mapping_network_optimizer = optim.Adam(self.mapping_network.parameters(), lr=self.mapping_network_learning_rate, betas=(0, 0.99), eps=1e-5)
             
         return discriminator_optimizer, generator_optimizer, mapping_network_optimizer
     
@@ -998,7 +1062,9 @@ class StyleGan2(LightningModule):
             real_images.requires_grad_()
         # Discriminator classification for real images
         real_output = self.discriminator(real_images)
-
+        
+        print("nans in output:", torch.sum(torch.isnan(real_output)))
+        
         # Get discriminator loss
         real_loss, fake_loss = self.discriminator_loss(real_output, fake_output)
         disc_loss = real_loss + fake_loss
@@ -1008,7 +1074,7 @@ class StyleGan2(LightningModule):
             # Calculate and log gradient penalty
             gp = self.gradient_penalty(real_images, real_output)
             # Multiply by coefficient and add gradient penalty
-#             print("gp:", gp)
+            print("gp:", gp)
             disc_loss = disc_loss + 0.5 * self.gradient_penalty_coefficient * gp * self.lazy_gradient_penalty_interval
 
         # Compute gradients