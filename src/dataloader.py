e2769bd77010ab1cee31310bcc2183ddc0e35099
Stephan Rasp <raspstephan@gmail.com>
2021-09-20 16:30:55 +0000

    Added HREF patches in dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -392,6 +392,160 @@ class TiggeMRMSDataset(Dataset):
         options.columns.name='TiggeMRMSDataset_Settings:'
         return options
 
+    
+    
+class TiggeMRMSHREFDataset(TiggeMRMSDataset):
+    def __init__(self, href_dir, **kwargs):
+        self.href = xr.open_mfdataset(href_dir)
+        self.href = self.href.tp.diff('lead_time').sel(lead_time=np.timedelta64(12, 'h'))
+        self.href['valid_time'] = self.href.init_time + self.href.lead_time
+        self.href = self.href.swap_dims({'init_time': 'valid_time'})
+        self.href.load()
+        
+        super().__init__(**kwargs)
+        
+    def _create_rqmask(self, rq_fn):
+        """Coarsen radar mask to patch and check for full coverage"""
+        rq = xr.open_dataarray(rq_fn).load()
+        # Account for padding
+#         rq = rq.isel(lat=slice(self.pad_mrms, -self.pad_mrms or None), lon=slice(self.pad_mrms, -self.pad_mrms or None))
+#         self.rqmask = rq.coarsen(lat=self.patch_mrms, lon=self.patch_mrms, boundary='trim').min() >= 0
+        # RQ mask checks for validity of patch indexed by lower left coordinate
+        # Note: lat is oriented reversele, so in "real" coords it's the upper left corner
+        self.rqmask = (rq[::-1, ::-1].rolling(
+            {'lat': self.patch_mrms}, min_periods=1
+        ).min().rolling(
+            {'lon': self.patch_mrms}, min_periods=1
+        ).min() >=0)[::-1, ::-1]
+        
+        hrefmask = np.isfinite(self.href).mean(('member', 'valid_time')) == 1
+        hrefmask = hrefmask.assign_coords(
+            {'lat': self.rqmask.lat.values, 'lon': self.rqmask.lon.values}
+        )
+        
+        self.rqmask *= hrefmask
+    
+    def _crop_times(self):
+        """Crop TIGGE and MRMS arrays to where they overlap"""
+        # Make TIGGE file have valid_time as dimension
+        valid_time = self.tigge.init_time + self.tigge.lead_time
+        self.tigge.coords['valid_time'] = valid_time
+        self.tigge = self.tigge.swap_dims({'init_time': 'valid_time'})
+
+        # Compute intersect
+        self.overlap_times = np.intersect1d(self.mrms.time, self.tigge.valid_time)
+
+        if self.first_days: # Only select first X days
+            dt = pd.to_datetime(self.overlap_times)
+            self.overlap_times = self.overlap_times[dt.day <= self.first_days]
+        if self.val_days: # Split into train and valid based on day of month
+            dt = pd.to_datetime(self.overlap_times)
+            self.overlap_times = self.overlap_times[
+                dt.day <= self.val_days if self.split == 'valid' else dt.day > self.val_days
+            ]
+            
+        self.overlap_times = np.intersect1d(
+            self.overlap_times, 
+            self.href.valid_time
+        )
+        
+        # Apply selection
+        self.mrms = self.mrms.sel(time=self.overlap_times)
+        self.tigge = self.tigge.sel(valid_time=self.overlap_times)
+        self.href = self.href.sel(valid_time=self.overlap_times)
+        
+    def __getitem__(self, idx, time_idx=None, full_array=False, no_cat=False,  member_idx=None):
+        """Return individual sample. idx is the sample id, i.e. the index of self.idxs.
+        X: TIGGE sample
+        y: corresponding MRMS (radar) sample
+        
+        **Attention:**
+        The self.tigge latitude variable is from ~50-20 degrees, i.e. not from small to large!
+        Be careful when transforming indices to actual latitude values! 
+        
+        """
+
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        time_idx_tmp, lat_idx, lon_idx = self.idxs[idx]
+        time_idx = time_idx or time_idx_tmp
+
+        # Get features for given time and patch
+        if full_array:  # Return full lats, lons
+            lat_slice = slice(0, None)
+            lon_slice = slice(0, None)
+        else:
+#             lat_slice = slice(lat_idx * self.patch_tigge, (lat_idx+1) * self.patch_tigge + self.pad_tigge*2)
+#             lon_slice = slice(lon_idx * self.patch_tigge, (lon_idx+1) * self.patch_tigge + self.pad_tigge*2)
+            lat_slice = slice(lat_idx-self.pad_tigge, lat_idx + self.patch_tigge + self.pad_tigge)
+            lon_slice = slice(lon_idx-self.pad_tigge, lon_idx + self.patch_tigge + self.pad_tigge)
+        X = self.tigge.isel(
+            valid_time=time_idx,
+            lat=lat_slice,
+            lon=lon_slice
+        )
+        if self.ensemble_mode == 'stack':
+            X = X.rename({'variable': 'raw_variable'}).stack(variable = ['raw_variable', 'member']).transpose(
+                'variable', 'lat', 'lon')
+        if self.ensemble_mode == 'random':
+            if member_idx is None:
+                member_idx = np.random.choice(self.tigge.member)
+            X = X.sel(member=member_idx)
+        if self.ensemble_mode == 'stack_by_variable':
+            X = xr.concat([X.rename({'variable': 'raw_variable'}).sel(raw_variable=self.var_names[i]).stack(variable=['member']).transpose(
+                'variable', 'lat', 'lon').drop('raw_variable') for i in self.tigge_vars if 'ens10' in i] + 
+           [X.sel(variable=[self.var_names[i] for i in self.tigge_vars if 'ens10' not in i], member=0).transpose(
+                'variable', 'lat', 'lon')], 
+          'variable')
+            
+            self.var_stack_idxs = {}
+            ind_count = 0
+            for i, var in enumerate(self.tigge_vars):
+                if 'ens10' in var:
+                    self.var_stack_idxs[self.var_names[var]] = ind_count + np.arange(10)
+                    ind_count+=10
+            for i, var in enumerate(self.tigge_vars):
+                if 'ens10' not in var:
+                    self.var_stack_idxs[self.var_names[var]] = ind_count + np.arange(1)
+                    ind_count+=1
+            
+        X = X.values
+        if hasattr(self, 'const'):  # Add constants
+            X = self._add_const(X, lat_slice, lon_slice)
+        
+
+        # Get targets
+        if full_array:   # Return corresponding MRMS slice; not used currently
+            lat_slice = slice(0, len(self.tigge.lat) * self.ratio)
+            lon_slice = slice(0, len(self.tigge.lon) * self.ratio)
+        else:
+            lat_slice = slice(lat_idx * self.ratio, lat_idx * self.ratio + self.patch_mrms)
+            lon_slice = slice(lon_idx * self.ratio, lon_idx * self.ratio + self.patch_mrms)
+        y = self.mrms.isel(
+            time=time_idx,
+            lat=lat_slice,
+            lon=lon_slice
+        ).values[None]  # Add dimension for channel
+        if self.pure_sr_ratio:
+            X = self._make_sr_X(y)
+        if self.cat_bins is not None and not no_cat:
+            y = self._categorize(y)
+            
+        href = self.href.isel(
+            valid_time=time_idx,
+            lat=lat_slice,
+            lon=lon_slice
+        ).values[None]
+            
+        if self.pad_tigge_channel:
+            X_crop = X[:,self.pad_tigge:self.pad_tigge + self.patch_tigge, self.pad_tigge:self.pad_tigge + self.patch_tigge]
+            X_downsample = resize(X[0:1,:,:], (1, self.patch_tigge, self.patch_tigge))
+            X = np.concatenate((X_crop, X_downsample), axis=0)
+            self.var_stack_idxs['pad_tigge_channel'] = ind_count + np.arange(1)
+            ind_count+=1
+            
+        return X.astype(np.float32), y.astype(np.float32), href.astype(np.float32)   # [vars, patch, patch]
+        
 
 def create_valid_predictions(model, ds_valid):
     # Get predictions for full field
